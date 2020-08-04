62280fcbc8e2ca334623dab351f6b3e1c90afd70
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-08-04 06:16:48 +0000

    Task 0: Adding a new node

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Adding a node to the binary tree
+ * @parent: The parent node
+ * @value: The payload
+ * Return: Pinter to the new node or NULL if failure
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	heap_t *node;
+
+	node = malloc(sizeof(heap_t));
+	if (node == NULL)
+		return (NULL);
+
+	node->n = value;
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
+
+	return (node);
+}
