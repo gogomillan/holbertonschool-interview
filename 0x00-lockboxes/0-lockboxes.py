3328dabf287a07c5a07e33ce98a3bbeb27d349ca
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-08-08 03:00:58 +0000

    Change algorithm

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,17 +4,38 @@ Script for the function canUnlockAll
 """
 
 
+def findKeys(boxes, each, rank):
+    """
+    Recurtion for the key in the boxes
+    """
+    """
+    print("R-{}|".format(boxes[each]), end= '')
+    r = input('')
+    """
+    if not boxes[each]:
+        """
+        print("(Empty)", end='')
+        """
+        return
+    for key in boxes[each]:
+        if rank.get(key) != key and key < len(boxes):
+            rank[key] = key
+            findKeys(boxes, key, rank)
+    
+
 def canUnlockAll(boxes):
     """
     Identify if all boxes can be opened
     """
     rank = {0: 0}
     each = 0
+    """
+    a = input(boxes)
+    """
     while each < len(boxes):
-        if not boxes[each] and each < (len(boxes) - 1):
+        if rank.get(each) != each:
             return False
-        for key in boxes[each]:
-            rank[key] = key
+        findKeys(boxes, each, rank)
         each += 1
 
     for each in range(0, len(boxes)):
