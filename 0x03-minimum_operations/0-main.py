b12172d3287a6e7d39e883dbbcdbfad074f52ca4
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-08-09 21:10:07 +0000

    Task 0: Minimum operations

--- 0x03-minimum_operations/0-main.py
+++ 0x03-minimum_operations/0-main.py
@@ -11,8 +11,69 @@ n = 12
 print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
 
 print ("===")
+n = -1
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+n = 0
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+n = 1
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+n = 2
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+n = 3
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+n = 4
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+n = 5
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+n = 6
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
 n = 7
 print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+n = 8
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+n = 9
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+n = 10
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+n = 11
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+n = 12
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+n = 13
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+n = 14
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+n = 15
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+n = 16
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+n = 17
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+n = 18
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+n = 19
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
 n = 20
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+n = 21
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+n = 22
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+n = 23
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+n = 24
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+n = 25
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+n = 26
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+n = 27
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+n = 28
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+n = 29
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+n = 30
 print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
 
--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -8,17 +8,17 @@ def minOperations(n):
     """
     Function to find the min operations
     """
-    if n < 1:
+    if n <= 1:
         return 0
 
-    min_o = n
-    divisor = 1
-    while divisor <= n:
-        if (n % divisor) == 0:
-            quotient = n // divisor
-            oper = divisor + quotient
-            if oper < min_o:
-                min_o = oper
-        divisor += 1
+    divisor = 2
+    oper = 0
+    quotient = n
+    while quotient > 1:
+        if (quotient % divisor) == 0:
+            quotient = quotient // divisor
+            oper += divisor
+        else:
+            divisor += 1
 
-    return min_o
+    return oper
