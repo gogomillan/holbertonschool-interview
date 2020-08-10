2d3e2d855b55588aa481eb94428ba6299fefa9f3
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-08-10 00:01:54 +0000

    Cleaning the mess

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -10,15 +10,16 @@ def canUnlockAll(boxes):
     """
     rank = {0: 0}
     each = 0
-    """
-    a = input(boxes)
-    """
+    """ Going through the boxes """
     while each < len(boxes):
+        """ Verifying if there is a key for this box """
         if rank.get(each) != each:
             return False
+        """ Getting the keys from the box, if there are """
         for key in boxes[each]:
             if key < len(boxes) and rank.get(key) != key:
                 rank[key] = key
+                """ For each key, verifying the matched box """
                 for box in boxes[key]:
                     if box < len(boxes) and rank.get(box) != box:
                         rank[box] = box
