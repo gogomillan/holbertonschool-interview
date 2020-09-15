a97924efa29998db72b67fccb70d8023c1ef91d6
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-09-15 04:22:22 +0000

    Adding comments to the code

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -10,16 +10,19 @@ int is_palindrome(unsigned long n)
 	char num[50];
 	int iter, total;
 
+	/* Put the number in array */
 	for (iter = 0; n; iter++)
 	{
 		num[iter] = n % 10;
 		n = n / 10;
 	}
 	num[iter] = '\0';
+	/* Go through the string to identify the palindrome */
 	total = iter;
-
 	for (iter = 0; iter < (total / 2); iter++)
+		/* If borders are diferent, then no palindrome */
 		if (num[iter] != num[total - 1 - iter])
 			return (0);
+	/* Else is a palindrome */
 	return (1);
 }
