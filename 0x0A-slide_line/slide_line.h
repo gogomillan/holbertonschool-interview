d88c145148e6cfe6a2f7236df17c020f043a9e7b
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-10-01 00:58:45 +0000

    Initial commit!

---
+++ 0x0A-slide_line/slide_line.h
@@ -0,0 +1,12 @@
+#ifndef GOGOMILLAN
+#define GOGOMILLAN
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define SLIDE_LEFT  1
+#define SLIDE_RIGHT 2
+
+int slide_line(int *line, size_t size, int direction);
+
+#endif /* GOGOMILLAN */
