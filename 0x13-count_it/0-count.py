8bacbf61aed8748be9743777bcab414197cff2c6
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-12-15 04:21:38 +0000

    Fix PEP8

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,53 +1,79 @@
 #!/usr/bin/python3
-"""keywords titles of all hot articles"""
+"""
+N queens
+"""
+
 import requests
-import sys
 
 
-def count_words(subreddit, word_list, kw_cont={}, next_pg=None, reap_kw={}):
-    """all hot posts by keyword"""
-    headers = {"User-Agent": "julgachancipa"}
+def count_words(subreddit, word_list):
+    """
+    Auxiliar function that invoques the funtion alt_count_words
 
-    if next_pg:
-        subRhot = requests.get('https://reddit.com/r/' + subreddit +
-                               '/hot.json?after=' + next_pg,
-                               headers=headers)
-    else:
-        subRhot = requests.get('https://reddit.com/r/' + subreddit +
-                               '/hot.json', headers=headers)
+    Args:
+        subreddit ([str]): [The subreddit to search]
+        word_list ([list]): [The list of words to search]
 
-    if subRhot.status_code == 404:
+    Returns:
+        [type]: [description]
+    """
+
+    instances = {}
+    after = ""
+    count = 0
+
+    return alt_count_words(subreddit, word_list, instances, after, count)
+
+
+def alt_count_words(subreddit, word_list, instances={}, after="", count=0):
+    """
+    recursive function that queries the Reddit API, parses the title of all
+    hot articles, and prints a sorted count of given keywords
+    (case-insensitive, delimited by spaces. Javascript should count as
+    javascript, but java should not)
+
+    Args:
+        subreddit ([str]): [The subreddit to search]
+        word_list ([list]): [The list of words to search]
+        instances (dict, optional): [Pairs of words - counts]. Defaults to {}.
+        after (str, optional): [parameter for the next page of the API result]
+                               Defaults to "".
+        count (int, optional): [Number of results matched]. Defaults to 0.
+    """
+
+    url = "https://www.reddit.com/r/" + subreddit + "/hot/.json"
+    h = {"User-Agent": "rodrigo_rca"}
+    p = {"after": after, "count": count, "limit": 100}
+
+    response = requests.get(url, headers=h, params=p, allow_redirects=False)
+    try:
+        rr = response.json()
+        if (response.status_code > 300):
+            raise BaseException
+    except BaseException:
         return
 
-    if kw_cont == {}:
+    rr = rr.get("data")
+    after = rr.get("after")
+    count += rr.get("dist")
+    for child in rr.get("children"):
+        title = child.get("data").get("title").lower().split()
         for word in word_list:
-            kw_cont[word] = 0
-            reap_kw[word] = word_list.count(word)
+            if (word.lower() in title):
+                t = len([t for t in title if t == word.lower()])
+                w = instances.get(word)
+                instances[word] = t if w is None else instances[word] + t
 
-    subRhot_dict = subRhot.json()
-    subRhot_data = subRhot_dict['data']
-    next_pg = subRhot_data['after']
-    subRhot_posts = subRhot_data['children']
-
-    for post in subRhot_posts:
-        post_data = post['data']
-        post_title = post_data['title']
-        title_words = post_title.split()
-        for w in title_words:
-            for key in kw_cont:
-                if w.lower() == key.lower():
-                    kw_cont[key] += 1
-
-    if next_pg:
-        count_words(subreddit, word_list, kw_cont, next_pg, reap_kw)
-
+    if (after is None):
+        if (len(instances) == 0):
+            print("")
+            return
+        code = []
+        for item, value in instances.items():
+            code.append((value, item))
+        code.sort(reverse=True)
+        for k in code:
+            item, value = k[0], k[1]
+            print("{}: {}".format(value, item))
     else:
-        for key, val in reap_kw.items():
-            if val > 1:
-                kw_cont[key] *= val
-
-        sorted_abc = sorted(kw_cont.items(), key=lambda x: x[0])
-        sorted_res = sorted(sorted_abc, key=lambda x: (-x[1], x[0]))
-        for res in sorted_res:
-            if res[1] > 0:
-                print('{}: {}'.format(res[0], res[1]))
+        alt_count_words(subreddit, word_list, instances, after, count)
