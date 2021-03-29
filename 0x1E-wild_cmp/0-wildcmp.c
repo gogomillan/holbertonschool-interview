3453f25069118867d79b46fc7d75fc23b9a57ac9
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2021-03-29 05:44:50 +0000

    Fix betty style

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -1,50 +1,41 @@
 #include "holberton.h"
 
-int existchar(char *str, char c);
-
 /**
- * wildcmp - function that compares two strings
- * @s1: String surce
- * @s2: String or pattern to find
- * Return: 1 if find it or 0 if not
- */
+* wildcmp - Compares 2 strings and returns 1 if the strings can be considered
+* identical, otherwise return 0.
+* @s1: 1st string
+* @s2: 2nd string. It may contain wildcard pattern *
+* Return: 1 if strings match, 0 otherwise
+*/
+
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && (*s2 == '\0' || (*s2 == '*' && *(s2 + 1) == '\0')))
-		return (TRUE);
-	else if (*s1 == '\0' || *s2 == '\0')
-		return (FALSE);
+	int cmp1, cmp2;
 
-	if (*s1 == *s2)
-		s1++, s2++;
-	else if (*s2 == '*' && *(s2 + 1) == '*')
-		s2++;
-	else if (*s1 != *s2 && *s2 == '*')
+	if (!*s1)
 	{
-		if (*s1 == *(s2 + 1) && !existchar(s1 + 1, *s1))
-			s2++;
-		else if (*(s1 + 1) == *(s2 + 1))
-			s2++, s1++;
-		else
-			s1++;
+		if (*s2 == '*')
+		{
+			cmp1 = wildcmp(s1, s2 + 1);
+			return (cmp1);
+		}
+
+		cmp2 = (*s2 == '\0') ? 1 : 0;
+
+		return (cmp2);
 	}
-	else if (*s1 != *s2)
-		return (FALSE);
 
-	return (wildcmp(s1, s2));
-}
+	if (*s2 == '*')
+	{
+		cmp1 = wildcmp(s1, s2 + 1);
+		cmp2 = wildcmp(s1 + 1, s2);
+		return (cmp1 || cmp2);
+	}
+	if (*s1 == *s2)
+	{
+		cmp1 = wildcmp(s1 + 1, s2 + 1);
+		return (cmp1);
+	}
 
-/**
- * existchar - function that look for a char in a string
- * @str: String surce
- * @c: Character to find
- * Return: 1 if find it or 0 if not
- */
-int existchar(char *str, char c)
-{
-	if (*str == '\0')
-		return (FALSE);
-	if (*str == c)
-		return (TRUE);
-	return (existchar (++str, c));
+	return (0);
 }
